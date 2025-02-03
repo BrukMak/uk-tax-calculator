30f38373ce5d64fac3ed34ab96419683761aed88
BrukMak <brukmakeni@gmail.com>
2025-02-03 23:10:42 +0300

    added the origin

--- backend/main.py
+++ backend/main.py
@@ -8,7 +8,7 @@ app = FastAPI()
 # Add CORS middleware
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],  # Allow requests from this origin
+    allow_origins=["https://uk-tax-calculator.vercel.app/"],  # Allow requests from this origin
     allow_credentials=True,
     allow_methods=["*"],  # Allow all methods (GET, POST, etc.)
     allow_headers=["*"],  # Allow all headers
